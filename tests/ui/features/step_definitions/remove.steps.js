e884ebacfa502080b906d1fe87465867ae36849f
Rudy <rudy.devisscher@gmail.com>
2015-12-09 16:35:02 +0100

    Click on button don't work

--- tests/ui/features/step_definitions/remove.steps.js
+++ tests/ui/features/step_definitions/remove.steps.js
@@ -14,6 +14,27 @@ module.exports = function () {
             assert.ok(_browser.success, 'page loaded : ok');
             assert.equal(_browser.text('h1'), 'Contacts');
 
+            var contacts = _browser.query('#contacts');
+            assert(contacts, 'contacts exist');
+
+            var table_contacts = _browser.query('#contacts table');
+            assert(table_contacts, 'table contacts exist');
+
+            var buttons_delete = _browser.query('#contacts table a');
+            assert(buttons_delete, 'buttons delete exist');
+
+            var it =  _browser.tabs.current.Contact.Contacts.instance().iterator();
+
+            var firstContact = it.next();
+
+            assert.equal(firstContact.firstName(), 'Eric');
+            assert.equal(firstContact.lastName(), 'RAMAT');
+
+            var idFirstContact = firstContact.id();
+
+            var buttons_delete_firstContact = _browser.query('#contacts a#button_'+idFirstContact);
+            assert(buttons_delete_firstContact, 'buttons delete first contact exist');
+
             callback();
         });
     });
@@ -23,11 +44,31 @@ module.exports = function () {
             if (err)
                 throw err;
 
+            var it =  _browser.tabs.current.Contact.Contacts.instance().iterator();
+
+            var idFirstContact;
+            var contact = it.next();
+
+            idFirstContact = contact.id();
+
+            _browser.clickLink('#contacts a#button_'+idFirstContact);
+
             callback();
         });
     });
 
     this.Then(/^This contact does not appear in the list$/, function (callback) {
-        callback();
+        _browser.visit('http://127.0.0.1:3000/', function (err) {
+            if (err)
+                throw err;
+
+            var it =  _browser.tabs.current.Contact.Contacts.instance().iterator();
+            var firstContact = it.next();
+
+            assert.not.equal(firstContact.firstName(), 'Eric');
+            assert.not.equal(firstContact.lastName(), 'RAMAT');
+
+            callback();
+        });
     });
 };
